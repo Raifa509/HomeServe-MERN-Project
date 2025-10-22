b2815171b6dcd187150e8ce54a534aaf3ea0bd2c
Raifa509 <fathimathulraifanp@gmail.com>
2025-10-22 14:10:35 +0530

    header changed

--- src/Users/components/Header.jsx
+++ src/Users/components/Header.jsx
@@ -1,108 +1,172 @@
-import { faUser } from '@fortawesome/free-regular-svg-icons'
-import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { faAddressCard, faUser } from '@fortawesome/free-regular-svg-icons'
+import { faBars, faPowerOff } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 function Header() {
   const [listStatus, setListStatus] = useState(false)
   const [token, setToken] = useState("")
   const [userDp, setUserDp] = useState("")
+  const [dropDownStatus, setDropDownStatus] = useState(false)
+  const navigate = useNavigate()
+
   useEffect(() => {
     if (sessionStorage.getItem("token")) {
       const token = sessionStorage.getItem("token")
       setToken(token)
-      const user = sessionStorage.getItem("user")
+      const user = JSON.parse(sessionStorage.getItem("user"))
       setUserDp(user.profile)
     }
   }, [])
 
+  const handleLogout = () => {
+    sessionStorage.clear()
+    setToken("")
+    setUserDp("")
+    setDropDownStatus(false)
+    navigate("/")
+  }
 
   return (
-    <>
+    <div className="w-full bg-green-900 md:h-15 text-white p-1">
+      <div className="md:flex justify-between items-center">
 
-      <div className="w-full bg-green-900 md:h-15 text-white p-1">
-        <div className="md:flex justify-between items-center">
-          {/* desktop logo */}
-          <div className='md:flex items-center ms-3 hidden'>
-            <img src="/logo.png" alt="" width={'70px'} />
-            <h2 className='headingFont text-2xl font-semibold text-yellow-400' style={{ marginLeft: '-7px', letterSpacing: '-1.5px' }}><Link to={'/'}>HomeServe</Link></h2>
-          </div>
-          {/* phone logo */}
-          <div className='flex items-center justify-center md:hidden'>
-            <img src="/logo.png" alt="" width={'50px'} />
-            <h2 className='headingFont text-xl font-semibold text-yellow-400' style={{ marginLeft: '-7px', letterSpacing: '-1.5px' }}><Link to={'/'}>HomeServe</Link></h2>
-          </div>
-
-          {/* nav list */}
-          <nav>
-            <div className='flex items-center justify-between p-3 md:hidden '>
-              <FontAwesomeIcon icon={faBars} size='xl' onClick={() => setListStatus(!listStatus)} />
-              <button className='bg-white text-green-600 shadow cursor-pointer rounded px-3 py-2 text-sm font-semibold hover:bg-green-800 hover:text-white border border-transparent hover:border-white'><FontAwesomeIcon icon={faUser} size='lg' />Login</button>
-            </div>
-            <div>
-              <ul className={listStatus ? 'flex flex-col' : 'md:flex justify-center items-center hidden'}>
-                <li className="relative group md:mx-2 mx-4 my-0.5">
-                  <Link to={'/'} className="hover:text-orange-300">
-                    Home
-                  </Link>
-                  <span className="absolute left-0 bottom-0 w-0 h-0.5 bg-orange-300 transition-all duration-300 group-hover:w-1/2"></span>
-                </li>
-
-                <li className="relative group md:mx-2 mx-4 my-0.5">
-                  <Link to={'/services'} className="hover:text-orange-300">
-                    Services
-                  </Link>
-                  <span className="absolute left-0 bottom-0 w-0 h-0.5 bg-orange-300 transition-all duration-300 group-hover:w-1/2"></span>
-                </li>
-                <li className="relative group md:mx-2 mx-4 my-0.5">
-                  <Link to={'/'} className="hover:text-orange-300">
-                    About us
-                  </Link>
-                  <span className="absolute left-0 bottom-0 w-0 h-0.5 bg-orange-300 transition-all duration-300 group-hover:w-1/2"></span>
-                </li>
-                <li className="relative group md:mx-2 mx-4 my-0.5">
-                  <Link to={'/careers'} className="hover:text-orange-300">
-                    Careers
-                  </Link>
-                  <span className="absolute left-0 bottom-0 w-0 h-0.5 bg-orange-300 transition-all duration-300 group-hover:w-1/2"></span>
-                </li>
-                <li className="relative group md:mx-2 mx-4 my-0.5">
-                  <Link to={'/'} className="hover:text-orange-300">
-                    Contact
-                  </Link>
-                  <span className="absolute left-0 bottom-0 w-0 h-0.5 bg-orange-300 transition-all duration-300 group-hover:w-1/2"></span>
-                </li>
-              </ul>
-
-            </div>
-          </nav>
-
-          {/* desktop buttons */}
-          <div className='md:flex items-center me-2 hidden'>
-
-
-
-            <Link to={'/booking'}> <button className='bg-yellow-400 text-white shadow cursor-pointer rounded px-3 py-2 text-sm font-semibold me-3 hover:bg-yellow-500'>Book Now</button></Link>
-            {
-              !token ?
-                <Link to={'/login'}>  <button className='bg-white text-green-600 shadow cursor-pointer rounded px-3 py-2 text-sm font-semibold hover:bg-green-800 hover:text-white border border-transparent hover:border-white'><FontAwesomeIcon icon={faUser} size='lg' />Login</button></Link>
-
-                :
-                <img src="/user.png" alt="" width={45} />
-
-            }
-
-
-
-
-
-
-          </div>
+        {/* Desktop logo */}
+        <div className='md:flex items-center ms-3 hidden'>
+          <img src="/logo.png" alt="" width={'70px'} />
+          <h2 className='headingFont text-2xl font-semibold text-yellow-400' style={{ marginLeft: '-7px', letterSpacing: '-1.5px' }}>
+            <Link to={'/'}>HomeServe</Link>
+          </h2>
         </div>
-      </div>
 
-    </>
+        {/* Mobile logo */}
+        <div className='flex items-center justify-center md:hidden'>
+          <img src="/logo.png" alt="" width={'50px'} />
+          <h2 className='headingFont text-xl font-semibold text-yellow-400' style={{ marginLeft: '-7px', letterSpacing: '-1.5px' }}>
+            <Link to={'/'}>HomeServe</Link>
+          </h2>
+        </div>
+
+        {/* Nav and Mobile Menu */}
+        <nav className='flex-1'>
+          <div className='flex items-center justify-between p-3 md:hidden'>
+
+            {/* Hamburger */}
+            <FontAwesomeIcon icon={faBars} size='xl' onClick={() => setListStatus(!listStatus)} />
+
+            {/* User / Login */}
+            {!token ? (
+              <Link to="/login">
+                <button className='bg-white text-green-600 shadow cursor-pointer rounded px-3 py-2 text-sm font-semibold hover:bg-green-800 hover:text-white border border-transparent hover:border-white'>
+                  <FontAwesomeIcon icon={faUser} size='lg' />Login
+                </button>
+              </Link>
+            ) : (
+              <div className="relative inline-block text-left">
+                <button
+                  onClick={() => setDropDownStatus(!dropDownStatus)}
+                  className="cursor-pointer"
+                >
+                  <img
+                    width={40}
+                    height={40}
+                    style={{ borderRadius: "50%" }}
+                    src={
+                      userDp === ""
+                        ? "/user.png"
+                        : userDp.startsWith("https://lh3.googleusercontent.com")
+                        ? userDp
+                        : "/user.png"
+                    }
+                    alt="user"
+                  />
+                </button>
+
+                {dropDownStatus && (
+                  <div className="absolute right-0 z-50 mt-2 w-40 origin-top-right rounded-md bg-green-50 shadow-lg">
+                    <div className="py-1">
+                      <Link to="/" className="block px-4 py-2 text-sm text-gray-700">
+                        <FontAwesomeIcon icon={faAddressCard} className="me-2" /> Profile
+                      </Link>
+                      <button
+                        onClick={handleLogout}
+                        className="block px-4 py-2 text-sm text-gray-700 cursor-pointer"
+                      >
+                        <FontAwesomeIcon icon={faPowerOff} className="me-2" /> Logout
+                      </button>
+                    </div>
+                  </div>
+                )}
+              </div>
+            )}
+          </div>
+
+          {/* Menu List */}
+          <ul className={listStatus ? 'flex flex-col' : 'md:flex justify-center items-center hidden'}>
+            <li className="relative group md:mx-2 mx-4 my-0.5">
+              <Link to={'/'} className="hover:text-orange-300">Home</Link>
+            </li>
+            <li className="relative group md:mx-2 mx-4 my-0.5">
+              <Link to={'/services'} className="hover:text-orange-300">Services</Link>
+            </li>
+            <li className="relative group md:mx-2 mx-4 my-0.5">
+              <Link to={'/'} className="hover:text-orange-300">About us</Link>
+            </li>
+            <li className="relative group md:mx-2 mx-4 my-0.5">
+              <Link to={'/careers'} className="hover:text-orange-300">Careers</Link>
+            </li>
+            <li className="relative group md:mx-2 mx-4 my-0.5">
+              <Link to={'/'} className="hover:text-orange-300">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* Desktop buttons */}
+        <div className='md:flex items-center me-2 hidden'>
+          <Link to={'/booking'}>
+            <button className='bg-yellow-400 text-white shadow cursor-pointer rounded px-3 py-2 text-sm font-semibold me-3 hover:bg-yellow-500'>Book Now</button>
+          </Link>
+
+          {!token ? (
+            <Link to="/login">
+              <button className="bg-white text-green-600 shadow cursor-pointer rounded px-3 py-2 text-sm font-semibold border me-3 hover:bg-transparent hover:text-white hover:border-white">
+                <FontAwesomeIcon icon={faUser} size="lg" /> Login
+              </button>
+            </Link>
+          ) : (
+            <div className="relative inline-block text-left">
+              <button onClick={() => setDropDownStatus(!dropDownStatus)} className="me-3 mt-1 cursor-pointer ">
+                <img
+                  width={45}
+                  height={45}
+                  style={{ borderRadius: "50%" }}
+                  src={userDp === "" ? "/user.png" : userDp.startsWith("https://lh3.googleusercontent.com") ? userDp : "/user.png"}
+                  alt="user"
+                />
+              </button>
+
+              {dropDownStatus && (
+                <div className="absolute right-0 z-50 mt-2 w-40 origin-top-right rounded-md bg-green-50 shadow-lg">
+                  <div className="py-1">
+                    <Link to="/" className="block px-4 py-2 text-sm text-gray-700">
+                      <FontAwesomeIcon icon={faAddressCard} className="me-2" /> Profile
+                    </Link>
+                    <button
+                      onClick={handleLogout}
+                      className="block px-4 py-2 text-sm text-gray-700 cursor-pointer"
+                    >
+                      <FontAwesomeIcon icon={faPowerOff} className="me-2" /> Logout
+                    </button>
+                  </div>
+                </div>
+              )}
+            </div>
+          )}
+        </div>
+
+      </div>
+    </div>
   )
 }
 
